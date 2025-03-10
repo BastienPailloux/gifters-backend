4ee308533dd022ca09601969e246a96aa146e6b5
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2025-03-10 10:12:05 +0100

    Group#index

---
+++ app/controllers/api/v1/groups_controller.rb
@@ -0,0 +1,39 @@
+module Api
+  module V1
+    class GroupsController < BaseController
+      before_action :authenticate_user!
+      before_action :set_group, only: [:show, :update, :destroy, :join, :leave]
+      before_action :ensure_member, only: [:show]
+      before_action :ensure_admin, only: [:update, :destroy]
+
+      # GET /api/v1/groups
+      def index
+        @groups = current_user.groups
+        render json: @groups, only: [:id, :name, :description, :invite_code]
+      end
+
+      private
+
+      def set_group
+        @group = Group.find_by(id: params[:id])
+        render json: { error: 'Group not found' }, status: :not_found unless @group
+      end
+
+      def ensure_member
+        unless @group && @group.users.include?(current_user)
+          render json: { error: 'You are not a member of this group' }, status: :forbidden
+        end
+      end
+
+      def ensure_admin
+        unless @group && @group.admin_users.include?(current_user)
+          render json: { error: 'You must be an admin to update this group' }, status: :forbidden
+        end
+      end
+
+      def group_params
+        params.require(:group).permit(:name, :description)
+      end
+    end
+  end
+end
