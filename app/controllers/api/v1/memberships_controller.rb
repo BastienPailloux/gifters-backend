475089c27af3149b30c5149c32804e48d94d43b1
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2025-03-10 12:12:01 +0100

    Ajout de la méthode create au MembershipsController

--- app/controllers/api/v1/memberships_controller.rb
+++ app/controllers/api/v1/memberships_controller.rb
@@ -2,8 +2,9 @@ module Api
   module V1
     class MembershipsController < BaseController
       before_action :set_group
-      before_action :ensure_user_in_group
+      before_action :ensure_user_in_group, except: [:create]
       before_action :set_membership, only: [:show]
+      before_action :ensure_admin, only: [:create]
 
       # GET /api/v1/groups/:group_id/memberships
       def index
@@ -16,6 +17,25 @@ module Api
       def show
         render json: @membership.as_json(include: { user: { only: [:id, :name, :email] } },
                                         methods: [:user_name, :user_email])
+      end
+
+      # POST /api/v1/groups/:group_id/memberships
+      def create
+        # Vérifier si l'utilisateur est membre du groupe avant de créer un nouveau membership
+        if params[:action] == 'create' && !current_user_membership
+          render json: { error: 'You are not a member of this group' }, status: :forbidden
+          return
+        end
+
+        @membership = @group.memberships.new(membership_params)
+
+        if @membership.save
+          render json: @membership.as_json(include: { user: { only: [:id, :name, :email] } },
+                                          methods: [:user_name, :user_email]),
+                 status: :created
+        else
+          render json: { errors: @membership.errors.full_messages }, status: :unprocessable_entity
+        end
       end
 
       private
@@ -43,8 +63,21 @@ module Api
         end
       end
 
+      def ensure_admin
+        unless current_user_membership&.admin?
+          action_name = params[:action] == 'create' ? 'add members to' :
+                        params[:action] == 'update' ? 'update memberships in' :
+                        'remove members from'
+          render json: { error: "You must be an admin to #{action_name} this group" }, status: :forbidden
+        end
+      end
+
       def current_user_membership
         @current_user_membership ||= @group.memberships.find_by(user: current_user)
+      end
+
+      def membership_params
+        params.require(:membership).permit(:user_id, :role)
       end
     end
   end
