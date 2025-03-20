b1bf4069f3eeae3772f3d9491c590cf0a898c722
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2025-03-20 08:41:46 +0100

    fix: remove user association from invitations and update mailer accordingly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_19_182034) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_20_073626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,11 +53,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_19_182034) do
     t.string "role", default: "member", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.index ["created_by_id"], name: "index_invitations_on_created_by_id"
     t.index ["group_id"], name: "index_invitations_on_group_id"
     t.index ["token"], name: "index_invitations_on_token", unique: true
-    t.index ["user_id"], name: "index_invitations_on_user_id"
   end
 
   create_table "jwt_denylists", force: :cascade do |t|
@@ -105,7 +103,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_19_182034) do
   add_foreign_key "gift_recipients", "gift_ideas"
   add_foreign_key "gift_recipients", "users"
   add_foreign_key "invitations", "groups"
-  add_foreign_key "invitations", "users"
   add_foreign_key "invitations", "users", column: "created_by_id"
   add_foreign_key "memberships", "groups"
   add_foreign_key "memberships", "users"
