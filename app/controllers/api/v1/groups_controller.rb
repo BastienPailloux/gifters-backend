a0dd767522621fc224fa8ca30b1bddf46bef56c0
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2025-10-24 18:24:53 +0200

    feat: adjust the group controller to take into account pundit

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -4,13 +4,12 @@ module Api
       before_action :set_children, only: [:index], if: -> { params[:with_children].present? }
       before_action :set_user, only: [:create, :show, :update, :destroy, :leave], if: -> { params[:user_id].present? }
       before_action :set_group, only: [:show, :update, :destroy, :leave]
-      before_action :ensure_member, only: [:show, :update, :destroy, :leave]
-      before_action :ensure_admin, only: [:update, :destroy]
+      before_action :authorize_group
 
       # GET /api/v1/groups
       def index
         @user = current_user
-        @groups = @user.groups
+        @groups = policy_scope(Group)
       end
 
       # GET /api/v1/groups/:id
@@ -48,12 +47,6 @@ module Api
 
       # DELETE /api/v1/groups/:id/leave
       def leave
-        # Vérifier si l'utilisateur est le dernier administrateur du groupe
-        if @group.admin_users == [current_user] || @group.admin_users == [@user]
-          render json: { error: 'You cannot leave the group as you are the last admin' }, status: :unprocessable_entity
-          return
-        end
-
         # Supprimer l'utilisateur du groupe
         membership = @group.memberships.find_by(user: @user || current_user)
         membership.destroy if membership
@@ -68,23 +61,6 @@ module Api
         render json: { error: 'Group not found' }, status: :not_found unless @group
       end
 
-      def ensure_member
-        unless @group && (@group.users.include?(current_user) || @group.users.include?(@user))
-          render json: { error: 'You are not a member of this group' }, status: :forbidden
-        end
-        true
-      end
-
-      def ensure_admin
-        return unless ensure_member
-
-        unless @group.admin_users.include?(current_user) || @group.admin_users.include?(@user)
-          action = action_name == 'update' ? 'update' : 'delete'
-          render json: { error: "You must be an admin to #{action} this group" }, status: :forbidden
-        end
-        true
-      end
-
       def group_params
         params.require(:group).permit(:name, :description)
       end
@@ -97,6 +73,17 @@ module Api
         @user = User.find(params[:user_id])
         unless current_user.can_access_as_parent?(@user)
           render json: { error: 'Forbidden: You are not the parent of this account' }, status: :forbidden
+        end
+      end
+
+      def authorize_group
+        case action_name
+        when 'index', 'create'
+          authorize Group
+        when 'show', 'update', 'destroy'
+          authorize @group
+        when 'leave'
+          authorize @group, :leave?
         end
       end
     end
