d69e7732e7efaadeb0a6e671832c29a01210f3f9
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2025-10-24 19:12:02 +0200

    feat: add policy to manage children

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -37,4 +37,24 @@ class UserPolicy < ApplicationPolicy
     # Seulement sa propre locale
     record.id == user.id
   end
+
+  # Méthodes pour la gestion des comptes enfants
+  def manage_as_child?
+    # Peut gérer un compte si on est le parent
+    user.can_access_as_parent?(record)
+  end
+
+  alias_method :show_child?, :manage_as_child?
+  alias_method :update_child?, :manage_as_child?
+  alias_method :destroy_child?, :manage_as_child?
+
+  def create_child?
+    # Tout utilisateur authentifié peut créer un compte enfant
+    true
+  end
+
+  def index_children?
+    # Tout utilisateur authentifié peut voir ses propres enfants
+    true
+  end
 end
